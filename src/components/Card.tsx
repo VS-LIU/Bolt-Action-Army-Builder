0234ac1a4eba6eb6463143db8fb209f46442daea
VS-LIU <vctl95@gmail.com>
2024-07-04 19:04:24 -0700

    Modify

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,18 +1,30 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+
+
+
 
 function Card() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  // useEffect(() => {
+  //   console.log('Count updated:', count);
+  // }, [count]);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
 
   return (
     <div className="card">
-      <button onClick={() => setCount((count) => count + 1)}>
+      <button onClick={handleClick}>
         count is {count}
       </button>
       <p>
         Edit <code>src/App.tsx</code> and save to test HMR
       </p>
     </div>
-  )
+  );
 }
+
 
 export default Card

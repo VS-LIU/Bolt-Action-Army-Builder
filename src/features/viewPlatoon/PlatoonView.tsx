b0c484359cd0c252cee8418f72f6f23f83867e79
VS-LIU <vctl95@gmail.com>
2024-07-04 22:56:39 -0700

    Create

---
+++ src/features/viewPlatoon/PlatoonView.tsx
@@ -0,0 +1,9 @@
+export default function PlatoonView(){
+    return (
+        <div className="border-grey-box">
+            <h2>Platoon View Container</h2>
+        </div>
+    )
+}
+
+// export default ListView
